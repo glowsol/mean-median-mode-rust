1de02f07b4ba203967638efc87a291e1a9d7fccd
glowsol <zahid@glowsol.com>
2020-01-11 19:21:29 +0500

    this version gets values from user.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
+use std::io;
 fn main() {
-    let mut list = vec![20,45,53,50,20];
+    let mut list = get_list();
     let mut total:u16 = 0;
     let mut count:u16 = 0;
     let mut hashm = HashMap::new();
@@ -26,4 +27,24 @@ fn main() {
     }
     println!("Mode:{}, count:{}",max_k,max_v);
 }
-
+fn get_list()->Vec<u16>{
+    let mut list:Vec<u16> = vec![];
+    println!("Please keep adding the list values, enter 'E' when done!");
+    loop{
+        let mut value = String::new();
+        io::stdin().read_line(&mut value).expect("Unexpected value intered!");
+        let value = value.trim();
+        match value{
+            "e"|"E"=>{return list;},
+            _=>()
+        };       
+        let value:u16 = match value.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>{
+                println!("Please enter a valid interger value");
+                continue;
+            }
+        };
+        list.push(value);
+    }
+}
